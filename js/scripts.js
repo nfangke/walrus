cf001806ac80860352f21329c36691914f7d0e22
Ashley Maceli and Nancy Fang <ashley@>
2016-03-22 09:28:36 -0700

    jQuery, JS

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $("#firststate").slideToggle();
+    $("#secondstate").slideToggle();
+});
+/*
+  $("h2").click(function() {
+    alert("HI");
+    });
+
+  $("ul").hover(function() {
+    alert("HOVERSTATE");
+  });*/
+
+});
